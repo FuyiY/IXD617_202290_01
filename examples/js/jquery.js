f165c64b79772b34d23892bde30c298d572828fb
FuyiY <fuyirose@gmail.com>
2022-09-19 15:44:24 -0700

    added js

--- examples/js/jquery.js
+++ examples/js/jquery.js
@@ -2,6 +2,7 @@
 
 $(()=>{
 
+    // ACCORDION
     $(".accordion dt").on("click", function(e) {
         // $(this).next().show();
 
@@ -14,5 +15,17 @@ $(()=>{
         $(this).next().slideDown()
             .siblings("dd").slideUp();
     });
+
+    /* TAB GROUP */
+    $(".tabgroup .tab").on("click", function(e) {
+        let id = $(this).index();
+
+        $(this).addClass("active")
+            .siblings().removeClass("active")
+
+        $(this).closest(".tabgroup")
+            .find(".content").eq(id).addClass("active")
+            .siblings().removeClass("active")
+    })
 
 });
